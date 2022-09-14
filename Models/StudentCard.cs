bc3e7bb5ceeb56973bce9984480b72949f00313c
Thorbjørn Hedegaard <drcamf@gmail.com>
2022-09-14 08:12:16 +0200

    Update from late
    Update to login tryouts

---
+++ Models/StudentCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkoleIT.Models
+{
+    public class StudentCard
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string studentIamge { get; set; }
+        public string userName { get; set; }
+        public string birthDate { get; set; }
+        public string school { get; set; }
+        public string endDate { get; set; }
+
+
+    }
+}
