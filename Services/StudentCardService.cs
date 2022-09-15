06c70ba9f93c8d923c0bebf419ed0f9d63a5f067
Thorbjørn Hedegaard <drcamf@gmail.com>
2022-09-15 09:35:33 +0200

    Create StudentCardService.cs

---
+++ Services/StudentCardService.cs
@@ -0,0 +1,40 @@
+﻿using SkoleIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkoleIT.Services
+{
+
+    public class StudentCardService
+    {
+        HttpClient httpClient;
+        public StudentCardService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        StudentCard studentCard = new();
+        public async Task<StudentCard> getStudentCard()
+        {
+            if(studentCard != null)
+            {
+                return studentCard;
+            }
+
+            var url = "https://svt.elthoro.dk/?pass=elev&item=card&id=1";
+
+            var response = await httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                studentCard = await response.Content.ReadFromJsonAsync<StudentCard>();
+            }
+
+            return studentCard;
+        }
+    }
+}
